bdb0aaeb7c97977699eaee58558171d7f34f0e00
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-03-14 17:26:57 +0000

    automatically backed up by learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -8,10 +8,11 @@ class Artist < ActiveRecord::Base
 
   def song_count
     #return the number of songs associated with the artist
-    self.songs.count 
+    self.songs.count
   end
 
   def genre_count
     #return the number of genres associated with the artist
+    self.genres.count 
   end
 end
